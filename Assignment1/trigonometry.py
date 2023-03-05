7856ff6e8da972c8816fadb7ad785987f857c0c9
RezaBarghamadi <rezabarghamadir@gmail.com>
2023-03-06 02:28:09 +0330

    Mpthalath

---
+++ Assignment1/trigonometry.py
@@ -0,0 +1,11 @@
+a=int(input("Enter The first size"))
+b=int(input("Enter The second size"))
+c=int(input("Enter The third size"))
+
+x=a+b
+y=a+c
+z=b+c
+if x>c and y>b and z>a and z>a:
+    print("Ok, let's draw")
+else:
+    print("I'm sorry, you can't draw it")
