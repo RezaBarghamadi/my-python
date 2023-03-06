09670fbd1a59d041ae6a445060e6637d24a761cc
RezaBarghamadi <rezabarghamadir@gmail.com>
2023-03-06 22:27:13 +0330

    calculator

---
+++ Assignment1/calculater.py
@@ -0,0 +1,67 @@
+import math
+import sys
+
+print("Hi Welcome to My Calculator :)")
+print("Are you ready to run this program")
+print("yes Or no")
+comment=input()
+if comment=="yes":
+    print("OK lets Go")
+else:
+    print("The End please Close")
+    sys.exit(0)
+
+
+print ("this List are: ")
+print("Computing ")
+print("+")
+print("-")
+print("/")
+print("*")
+print("^")
+print("sqr")
+print("sin")
+print("cos")
+print("tan")
+print("cot")
+print("fa")
+print ("enter your oprator")
+op=input()
+
+if op=="+" or op=="-" or op=="*" or op=="/":
+    a=float(input("Enter  first number :"))
+    b=float(input("Enter  second number :"))
+else:
+
+    a=float(input("Enter Number :"))
+
+if op=="+":
+    res=a+b
+elif op=="-":
+    res=a-b
+elif op=="*":
+    res=a*b
+elif op=="^":
+    res=a*a
+elif op=="sin":
+   res=math.sin(math.radians(a))
+elif op=="cos":
+   res=math.cos(math.radians(a))
+elif op=="tan":
+   res=math.tan(math.radians(a))
+elif op=="cot":
+   res=math.cot(math.radians(a))         
+elif op=="sqr":
+    res=math.sqrt(a)
+elif op=="fa":
+    res=math.factorial(a)
+elif op=="-":
+    res=a-b
+elif op=="/":
+    if b==0:
+        res="Your denominator is zero"
+else:
+    res=a/b
+
+
+print("Your action ",res)
