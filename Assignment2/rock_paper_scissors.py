54b0886fd67537438dd9b1e9afff59ee254cd97c
RezaBarghamadi <rezabarghamadir@gmail.com>
2023-04-07 15:45:40 +0430

    Time In Seconds

--- Assignment2/rock_paper_scissors.py
+++ Assignment2/rock_paper_scissors.py
@@ -1,11 +1,13 @@
 import random
 
 print ("Hi Welcome to this Game ")
+print ("This Game 1 =⚫ and 2=📄 and 3= ✂")
+print(" Enter 1 && 2 && 3")
 user_score=0
 computer_score=0
 
 x=random.randint(1,3)
-for i in range(3):
+while computer_score < 10 or user_score <10 :
     if x==1 :
         computer_choice="sang"
     elif x==2:
@@ -13,28 +15,25 @@ for i in range(3):
     elif x==3:
         computer_choice="ghychi"
 
-        user_choice=input()
-
-
-
+        user_choice=input(1,3)
         if computer_choice=="sang" and user_choice=="kaghaz ":
             user_choice=user_score + 1
-        # elif computer_choice=="ghychi" and user_choice=="sang" :
-        #     user_choice=user_choice + 1
-        # elif computer_choice=="kaghaz" and user_choice=="sang" :
-        #     computer_choice=computer_score + 1
-        # elif computer_choice=="kaghaz" and user_choice=="kaghaz" :
-        #      computer_choice=computer_score +0
-        # elif computer_choice=="ghychi" and user_choice=="ghychi" :
-        #     user_choice=user_score +0
-        # elif computer_choice=="sang" and user_choice=="sang" :
-        #     computer_choice=computer_score +0
-        # elif computer_choice=="kaghaz" and user_choice=="ghychi" :
-        #     user_choice=user_score +1
-        # elif computer_choice=="ghychi" and user_choice=="kaghaz" :
-        #     computer_choice=computer_score + 1
-        # elif computer_choice=="kaghaz" and user_choice=="sang" :
-        #     computer_choice=computer_score + 1
+        elif computer_choice=="ghychi" and user_choice=="sang" :
+            user_choice=user_choice + 1
+        elif computer_choice=="kaghaz" and user_choice=="sang" :
+            computer_choice=computer_score + 1
+        elif computer_choice=="kaghaz" and user_choice=="kaghaz" :
+             computer_choice=computer_score +0
+        elif computer_choice=="ghychi" and user_choice=="ghychi" :
+            user_choice=user_score +0
+        elif computer_choice=="sang" and user_choice=="sang" :
+            computer_choice=computer_score +0
+        elif computer_choice=="kaghaz" and user_choice=="ghychi" :
+            user_choice=user_score +1
+        elif computer_choice=="ghychi" and user_choice=="kaghaz" :
+            computer_choice=computer_score + 1
+        elif computer_choice=="kaghaz" and user_choice=="sang" :
+            computer_choice=computer_score + 1
         print("💻",computer_choice)
         print("👨",user_choice)
 
---
+++ Assignment2/time_in _seconds.py
@@ -0,0 +1,19 @@
+print ("The App Your Time in Seconds ")
+
+print ("Enter Your Houre ")
+hore =int(input("Your Houre is : " ))
+print(hore)
+
+print("Enter Your minutes")
+minutes =int(input("Your minutes is : " ))
+print(minutes)
+
+print("Enter Your Seconds")
+seconds =int(input("Your Seconds is : " ))
+print(seconds)
+
+hore_s=hore*3600
+minutes_s=minutes*60
+
+print("Your Tim is :",hore ,":",minutes,":",seconds)
+print("Your Time Equivalent :" ,hore_s+minutes_s +seconds ,"Seconds")
