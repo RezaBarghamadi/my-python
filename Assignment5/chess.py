11a9c8b6e194ef1e676612bb01aa994bc3bf0d2e
RezaBarghamadi <rezabarghamadir@gmail.com>
2023-11-05 12:52:17 +0330

    Update

--- Assignment5/chess.py
+++ Assignment5/chess.py
@@ -1,2 +1,20 @@
-def chess(m,n):
-    
+
+n=int(input("enter number of rows:"))
+m=int(input("enter number of coloumns:"))
+def chessboard(n,m):
+    for i in range(n):
+        if i%2==0:
+            for j in range(m):
+                if j%2==0:
+                    print("*", end=" ")
+                else:
+                    print("#", end=" ")
+            print()  
+        else:
+            for j in range(m):
+                if j%2==0:
+                    print("#", end=" ")
+                else:
+                    print("*", end=" ")
+            print()   
+chessboard(n,m)
