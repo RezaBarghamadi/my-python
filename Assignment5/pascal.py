9d3eddb18606efc01ae9c479cdb1805e1dc2ae90
RezaBarghamadi <rezabarghamadir@gmail.com>
2023-11-05 14:33:36 +0330

    pascal

---
+++ Assignment5/pascal.py
@@ -0,0 +1,9 @@
+a=int(input("Enter Numbeer :"))
+def pascal(n):
+   trow = [1]
+   y = [0]
+   for x in range(max(n,0)):
+      print(trow)
+      trow=[l+r for l,r in zip(trow+y, y+trow)]
+   return n>=1
+pascal(a) 
