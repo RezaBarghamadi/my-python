255ae68e77913c654f2b8a33689973994a177d0b
RezaBarghamadi <rezabarghamadir@gmail.com>
2023-11-12 22:17:45 +0330

    u

--- Assignment6/tictactoe.py
+++ Assignment6/tictactoe.py
@@ -1,13 +1,5 @@
 import pyfiglet
 from termcolor import colored
-
-def color():
-    Pel1=" X "
-    pel2=" O"
-
-    colored(Pel1, "red")
-    colored(pel2, "blue")
-
 def show():
     for row in game_chart:
         for cell in row :
@@ -16,16 +8,57 @@ def show():
         
 def check_player():
     if game_chart[0][0]=="X" and game_chart[0][1]=="X" and game_chart[0][2]=="X":
-     print("Player 1 id Win 😍")
-
+     print("Player 1 is Win 😍")
+     exit()
     if game_chart[1][0]=="X" and game_chart[1][1]=="X" and game_chart[1][2]=="X":
-     print("Player 1 id Win 😍")
-
+     print("Player 1 is Win 😍")
+     exit()   
     if game_chart[2][0]=="X" and game_chart[2][1]=="X" and game_chart[2][2]=="X":
-     print("Player 1 id Win 😍")
+     print("Player 1 is Win 😍")
+     exit()   
 
+    if game_chart[0][0]=="X" and game_chart[1][0]=="X" and game_chart[2][0]=="X":
+        print("Player 1 is Win 😍")
+        exit()
+    if game_chart[0][1]=="X" and game_chart[1][1]=="X" and game_chart[2][1]=="X":
+     print("Player 1 is Win 😍")
+     exit()   
+    if game_chart[0][2]=="X" and game_chart[1][2]=="X" and game_chart[2][2]=="X":
+     print("Player 1 is Win 😍")
+     exit()
 
+    if game_chart[0][0]=="X" and game_chart[1][1]=="X" and game_chart[2][2]=="X":
+     print("Player 1 is Win 😍")
+     exit()   
+    if game_chart[2][0]=="X" and game_chart[1][1]=="X" and game_chart[0][2]=="X":
+     print("Player 1 is Win 😍")
+     exit()   
 
+    if game_chart[0][0]=="O" and game_chart[0][1]=="O" and game_chart[0][2]=="O":
+     print("Player 2 is Win 😍")
+     exit()
+    if game_chart[1][0]=="O" and game_chart[1][1]=="O" and game_chart[1][2]=="O":
+     print("Player 2 is Win 😍")
+     exit()   
+    if game_chart[2][0]=="O" and game_chart[2][1]=="O" and game_chart[2][2]=="O":
+     print("Player 2 id Win 😍")
+        
+
+     if game_chart[0][0]=="O" and game_chart[1][0]=="O" and game_chart[2][0]=="O":
+        print("Player 2 id Win 😍")
+
+    if game_chart[1][1]=="O" and game_chart[1][1]=="O" and game_chart[2][1]=="O":
+     print("Player 2 id Win 😍")
+
+    if game_chart[0][2]=="O" and game_chart[1][2]=="O" and game_chart[2][2]=="O":
+     print("Player 2 id Win 😍")
+     exit()    
+    if game_chart[0][0]=="O" and game_chart[1][1]=="O" and game_chart[2][2]=="O":
+        print("Player 2 id Win 😍")
+        exit()       
+    if game_chart[2][0]=="O" and game_chart[1][1]=="O" and game_chart[0][2]=="O":
+     print("Player 2 id Win 😍")
+     exit() 
 
 title= pyfiglet.figlet_format("Tic Tak Toe",font="slant")
 print(colored(title, "yellow"), end='')
@@ -43,7 +76,6 @@ while True:
         if  0<= row <=2 and  0<=col <=2 : 
             if game_chart[row][col]== "-":
                 game_chart[row][col] = colored("X","red")
-                
                 break
             else:
                 print("Ghablan Entekhab Shodeh 😥 ")
@@ -51,6 +83,7 @@ while True:
             print("Plese Enter  0 && 1 && 2")        
     show()
     check_player()
+    
     print(colored("player 2  O :","blue"))
     while True:
         row=int(input("Enter Num row: "))
