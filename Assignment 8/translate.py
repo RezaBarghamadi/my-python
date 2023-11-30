21646dde16d9b8367bd8900e3ef6be6e591ee1f4
RezaBarghamadi <rezabarghamadir@gmail.com>
2023-12-01 00:31:01 +0330

    up

--- Assignment 8/translate.py
+++ Assignment 8/translate.py
@@ -13,7 +13,7 @@ def read_text_file():
 
     file.close()
 def translate_english_to_persian():
-    user_text=input("Enter Yor Word :")
+    user_text=input("Enter Yor English Word :")
 
     user_words=user_text.split(" ")
     output=""
@@ -28,7 +28,35 @@ def translate_english_to_persian():
             output=output + user_word + " "  
             
     print(output)  
+
+def translate_persian_to_english():
+
+    user_text=input("Enter Yor Persian Word :")
+
+    user_words=user_text.split(" ")
+    output=""
+
+    for user_word in user_words:
+        for word in words_bank:
+            if user_word == word["fa"]:
+                output=output+word["en"]+ " "
+                break
+
+        else:
+            output=output + user_word + " "  
+            
+    print(output) 
+def add_to_file():
+    en=input("Enter English Word : ")   
+    fa=input("Enter Translated  Word  Persian : ")    
+    file = open('translate.txt', 'a') 
+    file.write("\n")
+    file.write(en) 
+    file.write("\n")
+    file.write(fa)
+    file.close()
 def show_menu():
+    print("😍START APP 😍")
     print("Welcome To Translate 💻")
     print("1 - Translate English To persian ")
     print("2 - Translate persian To English")
@@ -43,10 +71,10 @@ while True:
         translate_english_to_persian()
 
     elif choice ==2:
-        ...
+        translate_persian_to_english()
 
     elif choice ==3:
-        ...
+        add_to_file()
 
     elif choice == 4:
         exit(0)
